8223b3d9b763cb6bc99d6f1c22a88f9a60531893
ubanerji <ubanerji@alumni.usc.edu>
2013-10-26 14:24:44 -0700

    Some UI

--- js/locationService.js
+++ js/locationService.js
@@ -4,19 +4,20 @@ var myLocation = {
 };
 
 function getLocation()
-  {
+{
   if (navigator.geolocation)
-    {
+  {
     navigator.geolocation.getCurrentPosition(showPosition);
-    }
+  }
   else{
       alert("Geolocation is not supported by this browser.");
   }
-  }
+}
+
 function showPosition(position)
-  {
+{
       lat = position.coords.latitude;
       lng = position.coords.longitude;
   alert("Latitude: " + position.coords.latitude + 
   "<br>Longitude: " + position.coords.longitude);	
-  }
+}
